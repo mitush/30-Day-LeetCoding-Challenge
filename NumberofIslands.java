c2e768d5dd46a41a3fdf8c704d8ddb08ec100953
mitush <mitushkushmith1@gmail.com>
2020-04-18 14:30:06 +0530

    Number of islands

---
+++ NumberofIslands.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid==null || grid.length==0)return 0;
+        int ans =0;
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1'){
+                    dfs(grid,i,j,m,n);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+    
+    public void dfs(char[][] grid,int i,int j,int m,int n){
+        if(i<0 || i>=m || j<0 || j>=n)return ;
+        if(grid[i][j]=='2' || grid[i][j]=='0')return;
+        grid[i][j] ='2';
+        dfs(grid,i-1,j,m,n);
+        dfs(grid,i+1,j,m,n);
+        dfs(grid,i,j-1,m,n);
+        dfs(grid,i,j+1,m,n);
+    }
+}
